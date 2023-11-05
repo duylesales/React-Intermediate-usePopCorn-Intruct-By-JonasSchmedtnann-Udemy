f3469e9788de148d1050d1c678867043180711f8
duylesales <80103322+duylesales@users.noreply.github.com>
2023-11-06 01:02:15 +0700

    async,loading State,selecting a movie,loading movie,add a watched movie,changing title,Clean up and listen to keypress

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rate {movieRating} stars</p>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
-    />
-    <StarRating size={24} />
-
-    <Test />
+    <App />
   </React.StrictMode>
 );
